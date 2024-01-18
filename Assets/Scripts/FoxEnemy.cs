259cb11470769fdd0bdaee88e2f9803a0ce9b3a1
Anna Schwegmann <anna.schwegmann@stud.uni-heidelberg.de>
2024-01-18 16:40:38 +0100

    abstract class Enemy, class FoxEnemy, WolfEnemy

---
+++ Assets/Scripts/FoxEnemy.cs
@@ -0,0 +1,22 @@
+using UnityEngine; 
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FoxEnemy : Enemy {
+
+    public FoxEnemy(){
+        currentHP = 10; 
+        attackRange = 2; 
+        damage = 3; 
+        followRange = 5; 
+        speed = 2.5; 
+        attackCooldown = 2; 
+
+    }
+
+    public virtual DropLoot(){
+
+    }
+}
---
+++ Assets/Scripts/WolfEnemy.cs
@@ -0,0 +1,22 @@
+using UnityEngine; 
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WolfEnemy : Enemy {
+
+    public WolfEnemy(){
+        currentHP = 15; 
+        attackRange = 3; 
+        damage = 4; 
+        followRange = 7; 
+        speed = 3; 
+        attackCooldown = 3; 
+
+    }
+
+    public virtual DropLoot(){
+        
+    }
+}
