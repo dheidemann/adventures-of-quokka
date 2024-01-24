67dcd085b278d355e8d8bb5e5ec4727e4b2d8708
Anna Schwegmann <anna.schwegmann@stud.uni-heidelberg.de>
2024-01-24 14:15:26 +0100

    Enemy Classes Fehlerbehebung

--- Assets/Scripts/FoxEnemy.cs
+++ Assets/Scripts/FoxEnemy.cs
@@ -11,12 +11,12 @@ public class FoxEnemy : Enemy {
         attackRange = 2; 
         damage = 3; 
         followRange = 5; 
-        speed = 2.5; 
+        speed = 2; 
         attackCooldown = 2; 
 
     }
 
-    public virtual DropLoot(){
+    public override void DropLoot(){
 
     }
 }
